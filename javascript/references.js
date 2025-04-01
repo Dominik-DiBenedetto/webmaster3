10ffda1ef58f41e848a790dab2e3b1856badc228
Dom <69871335+Dominik-DiBenedetto@users.noreply.github.com>
2025-03-31 21:22:52 -0400

    reference page added

---
+++ javascript/references.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector(".modal")
+const close_btn = document.querySelector(".close")
+
+const pdf_viewer = document.querySelector(".pdf-holder");
+const pdf_link = document.querySelector(".view-in-tab")
+
+const WORKLOGS_PATH = "/documents/testworklogs.pdf";
+const CHECKLIST_PATH = "/documents/CopyrightChecklist.pdf";
+
+const open_modal = (document) => {
+    if (document == "worklogs") {
+        pdf_viewer.src = WORKLOGS_PATH;
+        pdf_link.href = WORKLOGS_PATH;
+    } else if (document == "checklist") {
+        pdf_viewer.src = CHECKLIST_PATH;
+        pdf_link.href = CHECKLIST_PATH;
+    }
+    modal.classList.add("active");
+}
+
+close_btn.addEventListener("click", () => {
+    modal.classList.remove("active")
+})
